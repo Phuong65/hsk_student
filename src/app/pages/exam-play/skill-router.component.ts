690a9118cee67f6bc072df6fc9e0204efae79743
Phuong65 <145210443+Phuong65@users.noreply.github.com>
2026-08-06 09:31:50 +0700

    a

--- src/app/pages/exam-play/skill-router.component.ts
+++ src/app/pages/exam-play/skill-router.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { take } from 'rxjs/operators';
 
+import { ShiftTestService } from '@services/shift-test.service';
 import { ExamPlayComponent } from './children/exam-play/exam-play.component';
 import { SpeakingExamPlayComponent } from './children/speaking-exam-play/speaking-exam-play.component';
 
@@ -16,9 +17,18 @@ import { SpeakingExamPlayComponent } from './children/speaking-exam-play/speakin
 })
 export default class SkillRouterComponent implements OnInit {
     private route = inject(ActivatedRoute);
+    private shiftTestService = inject(ShiftTestService);
     component: Type<any> | null = null;
 
     ngOnInit(): void {
+        const shiftTestIdParam = this.route.snapshot.queryParamMap.get('shiftTestId') || '';
+        const shiftTestId = Number(shiftTestIdParam);
+        if (/^[1-9]\d*$/.test(shiftTestIdParam) && Number.isSafeInteger(shiftTestId)) {
+            this.shiftTestService.update(shiftTestId, { status: 1 }).pipe(take(1)).subscribe({
+                error: () => {},
+            });
+        }
+
         this.route.params.pipe(take(1)).subscribe(params => {
             const skill = params['skill'] || '';
             this.component = skill === 'speaking'
